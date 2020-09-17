1fe917b7a9e4cdf92fd23f24a412f324e0c0e1ce
svasquez <svasquez@controldiseno.com>
2020-09-17 16:29:57 -0500

    Refactor

--- angular-WishList/src/app/app.component.ts
+++ angular-WishList/src/app/app.component.ts
@@ -7,4 +7,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-WishList';
+
+  agregar(titulo:HTMLInputElement){
+    console.log(titulo.value);
+  }
 }
--- angular-WishList/src/app/lista-destinos/lista-destinos.component.ts
+++ angular-WishList/src/app/lista-destinos/lista-destinos.component.ts
@@ -1,3 +1,4 @@
+import { DestinoViaje } from './../Model/destino-viaje.model';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,13 +8,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListaDestinosComponent implements OnInit {
 
-  destinos:string[];
+  destinos: string[];
 
   constructor() {
-    this.destinos=['Buenos aires','Medellin',"Bogota","Barcelona","Quito"]
-   }
+    this.destinos = ['Buenos aires', 'Medellin', 'Bogota', 'Barcelona', 'Quito']
+  }
 
   ngOnInit(): void {
+  }
+
+  guardar(nombre: string, url: string): boolean {
+
+    const destino = new DestinoViaje(nombre, url);
+    console.log(destino);
+    return false;
   }
 
 }
